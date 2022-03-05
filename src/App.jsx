0f4fd68e46566fe05336979dd50ee464ac01405a
tamalweb <tamalweb@protonmail.com>
2022-03-05 17:59:57 +0600

    Updates to next event in Mar 26

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-import { useState } from 'react'
-import logo from './img/logo.jpg'
-import './App.css'
-import './style/main.css'
-import Countdown from 'react-countdown'
-import GitHubButton from 'react-github-btn'
-
-const event = {
-  TITLE: 'How To Have Impactful 1:1 Meetings - With Alex & Rahul',
-  TIME_GMT: '26 Feb 2022 18:00:00 GMT',
-  DETAILS: `Hi, we are Alex and Rahul, 2 tech leads who have ~16 years of experience combined working for top Silicon Valley software companies like Facebook, Pinterest, and Robinhood. This free event is part of a broader tech community where our mission is to lower barriers in tech for all.
-
-  We will be talking about one of the most crucial building blocks of a successful tech career: The 1 on 1 meeting. We have had literally thousands of these across our careers, and we have learned that there is a sky high skill ceiling for them. 1:1s are an engine for so many helpful things like building relationships, onboarding, transferring knowledge, resolving disputes, and so much more. If you want to become a top software engineer or really just a top tech worker in general, you absolutely HAVE to master the 1:1 meeting. And that is exactly what we're going to help you do with this session!`,
-  ZOOM_LINK:
-    'https://us02web.zoom.us/j/89046059279?pwd=a2daZU8wUngyelpjdjRHOVVLWEhTUT09',
-  YOUTUBE_LINK: 'https://www.youtube.com/c/RahulPandeyrkp/videos',
-  GOOGLE_CAL:
-    'https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20220226T180000Z%2F20220226T200000Z&details=For%20details%2C%20visit%20https%3A%2F%2Fwww.linkedin.com%2Fevents%2F6887512898000830464%2Fabout%2F%0A%0AHi%2C%20we%20are%20Alex%20and%20Rahul%2C%202%20tech%20leads%20who%20have%20~16%20years%20of%20experience%20combined%20working%20for%20top%20Si%E2%80%A6&text=How%20To%20Have%20Impactful%201%3A1%20Meetings%20-%20With%20Alex%20%26%20Rahul&location=https%3A%2F%2Fwww.linkedin.com%2Fevents%2F6887512898000830464%2Fabout%2F',
-  OUTLOOK_CAL:
-    'https://outlook.office.com/calendar/0/deeplink/compose?startdt=2022-02-26T18%3A00%3A00Z&enddt=2022-02-26T20%3A00%3A00Z&subject=How%20To%20Have%20Impactful%201%3A1%20Meetings%20-%20With%20Alex%20%26%20Rahul&body=For%20details%2C%20visit%20https%3A%2F%2Fwww.linkedin.com%2Fevents%2F6887512898000830464%2Fabout%2F%0A%0AHi%2C%20we%20are%20Alex%20and%20Rahul%2C%202%20tech%20leads%20who%20have%20~16%20years%20of%20experience%20combined%20working%20for%20top%20Si%E2%80%A6&location=https%3A%2F%2Fwww.linkedin.com%2Fevents%2F6887512898000830464%2Fabout%2F',
-  YAHOO_CAL:
-    'https://calendar.yahoo.com/?v=60&TYPE=20&VIEW=d&DESC=For%20details%2C%20visit%20https%3A%2F%2Fwww.linkedin.com%2Fevents%2F6887512898000830464%2Fabout%2F%0A%0AHi%2C%20we%20are%20Alex%20and%20Rahul%2C%202%20tech%20leads%20who%20have%20~16%20years%20of%20experience%20combined%20working%20for%20top%20Si%E2%80%A6&ST=20220226T180000Z&ET=20220226T200000Z&TITLE=How%20To%20Have%20Impactful%201%3A1%20Meetings%20-%20With%20Alex%20%26%20Rahul&URL=https%3A%2F%2Fwww.linkedin.com%2Fevents%2F6887512898000830464%2Fabout%2F&in_loc=https%3A%2F%2Fwww.linkedin.com%2Fevents%2F6887512898000830464%2Fabout%2F',
-}
+import { useState } from "react"
+import logo from "./img/logo.jpg"
+import "./App.css"
+import "./style/main.css"
+import Countdown from "react-countdown"
+import GitHubButton from "react-github-btn"
+import event from "./event"
 
 const Completionist = () => <span>This event is currently happening</span>
 
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
-  const dayLabel = days === 1 ? 'Day' : 'Days'
+  const dayLabel = days === 1 ? "Day" : "Days"
 
   if (completed) {
     // Render a completed state
@@ -36,7 +20,7 @@ const renderer = ({ days, hours, minutes, seconds, completed }) => {
       <span>
         {days ? (
           <span>
-            {days} {dayLabel} :{' '}
+            {days} {dayLabel} :{" "}
           </span>
         ) : null}
         {hours ? <span>{hours} hrs : </span> : null}
@@ -50,7 +34,7 @@ const renderer = ({ days, hours, minutes, seconds, completed }) => {
 function App() {
   const [count, setCount] = useState(0)
 
-  const timeToNextEvent = Date.parse(event.TIME_GMT)
+  const timeToNextEvent = Date.parse(event.DATE_TIME)
   const timeToTestApp = Date.now() + 60000 * 60 * 24 * 3
 
   return (
@@ -82,10 +66,10 @@ function App() {
           <div className="event__title">Event Title: {event.TITLE}</div>
           <div className="event__date">
             <strong>Event Date: </strong>
-            {new Date(event.TIME_GMT).toLocaleString('en-US', {
-              timeZoneName: 'short',
-            })}{' '}
-            /{' '}
+            {new Date(event.DATE_TIME).toLocaleString("en-US", {
+              timeZoneName: "short",
+            })}{" "}
+            /{" "}
             <a href={event.GOOGLE_CAL} target="_blank">
               Add to Google Calendar
             </a>
@@ -130,7 +114,7 @@ function App() {
       </div>
       <footer className="footer">
         <span>
-          &copy; 2022 Developed by{' '}
+          &copy; 2022 Developed by{" "}
           <a href="https://twitter.com/tamalweb">Tamal Web</a>
         </span>
         <GitHubButton
---
+++ src/event.js
@@ -0,0 +1,14 @@
+const event = {
+  TITLE: "How To Do Amazing Code Reviews - With Alex & Rahul",
+  DATE_TIME: "26 Mar 2022 9:00:00 PST",
+  DETAILS: `Hi, we are Alex and Rahul, 2 tech leads who have ~16 years of experience combined working for top Silicon Valley software companies like Facebook, Pinterest, and Robinhood. This free event is part of a broader tech community where our mission is to lower barriers in tech for all.
+
+  When done properly, code review is one of the most powerful tools you have access to, both for your own learning and building the necessary influence to become a senior engineer/tech lead. The problem is that a lot of teams and companies don't do it properly. For junior engineers in particular, a good code review culture is vital towards their development, evolving their ability to write scuffed, hacky code to truly scalable production code. Whether you're looking to review others' code more effectively or get better code review on your own code, please join us for this in-depth session!`,
+  ZOOM_LINK:
+    "https://us02web.zoom.us/j/83082884598?pwd=VHlYRnUrZDQ5bnp0YzZBSjh2SGFQQT09",
+  YOUTUBE_LINK: "https://www.youtube.com/c/RahulPandeyrkp/videos",
+  GOOGLE_CAL:
+    "https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20220326T170000Z%2F20220326T183000Z&details=For%20details%2C%20visit%20https%3A%2F%2Fwww.linkedin.com%2Fevents%2Fhowtodoamazingcodereviews-witha6902002352128167936%2F%0A%0AHi%2C%20we%20are%20Alex%20and%20Rahul%2C%202%20tech%20leads%20who%20have%20~16%20years%20of%20experience%20combined%20working%20for%20top%20Si%E2%80%A6&text=How%20To%20Do%20Amazing%20Code%20Reviews%20-%20With%20Alex%20%26%20Rahul&location=https%3A%2F%2Fwww.linkedin.com%2Fevents%2Fhowtodoamazingcodereviews-witha6902002352128167936%2F",
+}
+
+export default event
