50c6db222d85edb3da13b8d7b8571a27fb7d3e6a
tamalweb <tamalweb@protonmail.com>
2022-04-21 22:49:53 +0600

    Adds firebase and configures the SDK

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -9,6 +9,27 @@ import { Routes, Route, Link } from "react-router-dom"
 import Home from "./Home"
 import Dashboard from "./Dashboard"
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getDatabase } from "firebase/database"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbgDJM0Fhrzo6Omo0hTtHEphcqpEEfBKc",
+  authDomain: "tcg-event.firebaseapp.com",
+  databaseURL: "https://tcg-event-default-rtdb.firebaseio.com",
+  projectId: "tcg-event",
+  storageBucket: "tcg-event.appspot.com",
+  messagingSenderId: "149216901118",
+  appId: "1:149216901118:web:b7ae5d551c85e16afef2af",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+
 function Index() {
   const [count, setCount] = useState(0)
 
@@ -19,9 +40,13 @@ function Index() {
     <>
       <div className="wrapper">
         <header className="header">
-          <img src={logo} alt="Tech Career Growth Logo" />
+          <Link to="/">
+            <img src={logo} alt="Tech Career Growth Logo" />
+          </Link>
           <div>
-            <h1>Tech Career Growth</h1>
+            <h1>
+              <Link to="/">Tech Career Growth</Link>
+            </h1>
             <p>Next Event Countdown</p>
           </div>
         </header>
